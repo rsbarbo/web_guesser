f781f2038973194db777c7c8088f0790e74330cc
Raphael Barbo <rsbarbo@gmail.com>
2016-08-08 19:07:02 -0600

    updated webguesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,25 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-NUMBER = rand(100)
-@@counter = 5
+  @@counter = 5
 
-get '/' do
-  guess = params["guess"].to_i
-  background, message = check_guess(guess)
-  erb :index, :locals => {:number => NUMBER, :message => message, :background => background}
-end
+  NUMBER = rand(100)
 
-def check_guess(guess)
-  @@counter -= 1
-  return ["purple", "You have guessed more than 5 times now, the correct number was #{NUMBER}", end_game] if @@counter == 0
-  return ["green","You have not guessed a number, try again"] if guess == nil || guess == 0
-  return ["red", "Way Too high!"] if !guess.nil? && guess != nil && guess != 0 && guess > (NUMBER + 5)
-  return ["red", "Way Too low!"] if !guess.nil? && guess != nil && guess != 0 && guess < (NUMBER - 5)
-  return ["brown", "Too high!"] if !guess.nil? && guess != nil && guess != 0 && guess > NUMBER
-  return ["brown", "Too low!"] if !guess.nil? && guess != nil && guess != 0 && guess < NUMBER
-end
+  get '/' do
+    guess = params["guess"].to_i
+    # cheat = params["cheat"]
+    background, message = check_guess(guess)
+    erb :index, :locals => {:number => NUMBER, :message => message, :background => background}
+  end
 
-def end_game
-  Process.kill("TERM", Process.pid)
-end
+  def check_guess(guess)
+    @@counter -= 1
+    return ["purple", "You have guessed more than 5 times now, the correct number was #{NUMBER}", end_game] if @@counter == 0
+    return ["green","You have not guessed a number, try again"] if guess == nil || guess == 0
+    return ["red", "Way Too high!"] if !guess.nil? && guess != nil && guess != 0 && guess > (NUMBER + 5)
+    return ["red", "Way Too low!"] if !guess.nil? && guess != nil && guess != 0 && guess < (NUMBER - 5)
+    return ["brown", "Too high!"] if !guess.nil? && guess != nil && guess != 0 && guess > NUMBER
+    return ["brown", "Too low!"] if !guess.nil? && guess != nil && guess != 0 && guess < NUMBER
+  end
+
+  def end_game
+    Process.kill("TERM", Process.pid)
+  end
