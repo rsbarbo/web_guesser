46e140d4595c96c7f895b2de9f6d9cb9e75a0514
Raphael Barbo <rsbarbo@gmail.com>
2016-08-08 18:47:39 -0600

    the game shuts down after 5 tries

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
+
 NUMBER = rand(100)
+@@counter = 5
 
 get '/' do
   guess = params["guess"].to_i
@@ -10,9 +12,15 @@ get '/' do
 end
 
 def check_guess(guess)
+  @@counter -= 1
+  return ["purple", "You have guessed more than 5 times now, the correct number was #{NUMBER}", end_game] if @@counter == 0
   return ["green","You have not guessed a number, try again"] if guess == nil || guess == 0
   return ["red", "Way Too high!"] if !guess.nil? && guess != nil && guess != 0 && guess > (NUMBER + 5)
   return ["red", "Way Too low!"] if !guess.nil? && guess != nil && guess != 0 && guess < (NUMBER - 5)
   return ["brown", "Too high!"] if !guess.nil? && guess != nil && guess != 0 && guess > NUMBER
   return ["brown", "Too low!"] if !guess.nil? && guess != nil && guess != 0 && guess < NUMBER
+end
+
+def end_game
+  Process.kill("TERM", Process.pid)
 end
